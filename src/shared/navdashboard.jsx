844930dd457d1bda3ba967e22bf650dd4cbfe7bb
dipu59 <12biswasdipu@gmail.com>
2025-04-30 20:21:49 +0530

    Nav Dashboard Responsive Completed

--- src/shared/navdashboard.jsx
+++ src/shared/navdashboard.jsx
@@ -7,24 +7,28 @@ import { Button } from "../common/button";
 const Navdashboard = () => {
   return (
     <main className=" bg-[#E4E4E780]/50 ">
-      <div className=" max-w-[1268px] px-5 lg:px-0 mx-auto flex flex-col lg:flex-row justify-between items-center pb-[23px]">
-        <div>
+      <div className=" max-w-[1268px] px-5 lg:px-0 mx-auto flex flex-col lg:flex-row lg:justify-between  lg:items-center pb-[23px]">
+        <div className="py-4 lg:py-0">
           <Disc className="text-[12px]">Welcome Back</Disc>
           <Title>Dashboard</Title>
         </div>
-        <div className="flex flex-wrap gap-[14px]">
-          <div className="text-[#71717A] text-sm flex items-center justify-center border border-[#E4E4E7]  gap-[5px] pl-[10px] active:ring-2 hover:ring-2  max-w-[190px]   rounded-[6px] bg-[#FAFAFA]/50 ">
-            <Search className="size-[18px]" />
-            <input
-              placeholder="Search incident"
-              type="text"
-              className=" outline-none py-[17px] max-w-[157px]  h-[42px] bg-[#FAFAFA]/50"
-            />
+        <div className="flex flex-wrap gap-[14px]  ">
+          <div>
+            <div className="text-[#71717A] text-sm flex items-center justify-center border border-[#E4E4E7]  gap-[5px] pl-[10px] active:ring-2 hover:ring-2  max-w-[190px]   rounded-[6px] bg-[#FAFAFA]/50 ">
+              <Search className="size-[18px]" />
+              <input
+                placeholder="Search incident"
+                type="text"
+                className=" outline-none py-[0px] max-w-[157px]  h-[42px] bg-[#FAFAFA]/50"
+              />
+            </div>
           </div>
           <button className="w-[148px] h-[42px] bg-[#FAFAFA]/50 border border-[#E4E4E7] rounded-[6px] text-[12px] text-[#A1A1AA] active:scale-95 transition-all shadow ">
             Sort By: Date modified
           </button>
-          <Button className='py-0 px-[25px] '>Cypher AI</Button>
+          <Button className="lg:py-0 w-full  px-[25px] lg:h-[42px] md:w-fit ">
+            Cypher AI
+          </Button>
         </div>
       </div>
     </main>
